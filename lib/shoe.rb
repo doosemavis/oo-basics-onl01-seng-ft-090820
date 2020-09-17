0eed078b6283640207816a1ef7bb54c896388388
Moose Davis <moosedavis2011@gmail.com>
2020-09-17 19:23:15 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
 class Shoe
-  attr_accessor :brand, :color, :size, :material, :condition
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
   
-  
-  def initialize(shoe)
-    @shoe = shoe 
+  def initialize(brand)
+    @brand = brand
   end
+  
+  def cobble
+    puts "Repaired"
+  
   
 end 
